66a2d567a0be40f21a2feaf897d7868ac02ccd75
Benjamin Sternthal <ben@devpatch.com>
2015-07-17 11:05:15 -0700

    Cleanup on Aisle 7

--- flightplan.js
+++ flightplan.js
@@ -1,35 +1,24 @@
-var Flightplan = require('flightplan');
+/* Example usage fly production */
+
+var plan = require('flightplan');
 var deploy = require('./deploy');
 
-var plan = new Flightplan();
-// configuration
-plan.briefing({
-  debug: true
-});
-
 // run commands on localhost
+plan.target('production', [
+  {
+    host: deploy.host,
+    username: deploy.username,
+    agent: process.env.SSH_AUTH_SOCK,
+    path: deploy.path
+  }
+]);
+
 plan.local(function(local) {
   var rsyncPath = deploy.username + "@" + deploy.host + ":" + deploy.path;
 
   local.log('Build Site Using Wintersmith');
-  local.exec('wintersmith build')
+  local.exec('wintersmith build');
 
   local.log('SCP Files');
   local.exec('rsync -avc --delete build/ ' + rsyncPath);
-
-});
-
-
-// executed if flightplan succeeded
-plan.success(function() {
-  console.log('Success');
-});
-
-// executed if flightplan failed
-plan.disaster(function() {
-  console.log('Disaster');
-});
-
-plan.debriefing(function() {
-  console.log('Exiting ');
 });
